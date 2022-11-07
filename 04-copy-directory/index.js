d6ef940a54e2c10af613cbe24296855d35cbb9cb
DmitriyYaskevich <kunegz@gmail.com>
2022-11-07 17:52:37 +0300

    init:task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const dir = path.join(__dirname, 'files');
+const copyFile = path.join(__dirname, 'files-copy');
 
+function copyDir(dir, copyFile) {
+  fs.rm(copyFile, { force: true, recursive: true }, () => {
+
+    fs.mkdir(copyFile, { recursive: true }, () => { });
+
+    fs.readdir(dir, { withFileTypes: true }, (err, data) => {
+      if (err) {
+        console.log('Error:', err.message);
+      } else {
+        data.forEach(dat => {
+          if (dat.isFile()) {
+            fs.copyFile(path.join(dir, dat.name), path.join(copyFile, dat.name), () => { });
+          } else if (dat.isDirectory()) {
+            copyDir(path.join(dir, dat.name), path.join(copyFile, dat.name));
+          }
+        });
+      }
+    });
+  });
+}
+
+copyDir(dir, copyFile);
